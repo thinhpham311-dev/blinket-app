57d4f768d5cebd164850c1a9a87a7dabeac80637
thinhpham <thinhpham67ag@gmail.com>
2024-09-01 22:17:28 +0700

    update code

--- babel.config.js
+++ babel.config.js
@@ -1,3 +1,17 @@
 module.exports = {
   presets: ['module:@react-native/babel-preset'],
+  plugins: [
+    'module-resolver',{
+      root: "./src",
+      alias: {
+      "@assets": ["./src/assets"],
+      "@features": ["./src/features"],
+      "@service": ["./src/service"],
+      "@navigation": ["./src/navigation"],
+      "@components": ["./src/components"],
+      "@state": ["./src/state"],
+      "@utils": ["./src/utils"],
+      }
+    }
+  ] 
 };
